d9738806fba85f6148467d237b635885388772e0
captcontrol <pridham.tom@gmail.com>
2016-11-11 23:02:13 -0700

    added services and html templates

---
+++ public/js/services/service2.js
@@ -0,0 +1,16 @@
+"use strict";
+angular.module('app').service('service2', function ($scope, $http, $q) {
+    this.myAsyncFunction = function(){
+        var defer = $q.defer();
+        $http.get('/someDataurl')
+            .success(function(data){
+                $scope.data = data;
+                defer.resolve(data);
+            })
+            .error(function(err){
+                alert(err);
+                defer.reject(err);
+            });
+        return defer.promise;
+    }
+});
