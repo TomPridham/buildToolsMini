ab5eb7423e4ce49c70862c4530b684cb2db0b131
captcontrol <pridham.tom@gmail.com>
2016-11-11 22:50:34 -0700

    added controllers and routing

---
+++ public/js/controllers/homeCtrl.js
@@ -0,0 +1,13 @@
+"use strict";
+angular.module('app').controller('homeCtrl', function ($state, service1, service2, service3) {
+    $state.whatever = ['some junk'];
+
+    $state.myFunc = function(){
+        alert('hi');
+        console.log('bruh');
+        //do other things.
+    };
+    $state.myOtherfunction= function(){
+        alert('This is the other function')
+    }
+});
